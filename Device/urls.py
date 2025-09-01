d1d9a36cd74dad81e1c2f8f3f2d74e59b07ec15b
y.uchimoto0403@gmail.com <y.uchimoto0403@gmail.com>
2025-09-01 17:00:16 +0900

    一旦

--- Device/urls.py
+++ Device/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Device.viewslist import views_device, views_login, views_home
+from Device.viewslist import views_device, views_login, views_home, views_admin, views_customer
 urlpatterns = [
     #--- 申請一覧 ---#
     path('', views_login.login, name='login'),
@@ -10,7 +10,17 @@ urlpatterns = [
 
     path('detail_device/<int:intUsr>/<int:intDevice>/', views_device.detail_device, name='detail_device'),
 
-    path('admin_manage/<int:intUsr>/', views_admin.admin_manage, name='manage_admin'),
+    path('manage_admin/<int:intUsr>/', views_admin.manage_admin, name='manage_admin'),
+
+    #path('device_manage/<int:intUsr>/', views_device.device_manage, name='manage_device'),
+
+    #path('create_device/<int:intUsr>/', views_device.create_device, name='create_device'),
+
+    #path('edit_device/<int:intUsr>/', views_device.edit_device, name='edit_device'),
+
+    #path('customer_manage/<int:intUsr>/', views_customer.manage_customer, name='manage_customer'),
+
+
 
  
 
--- Device/viewslist/views_admin.py
+++ Device/viewslist/views_admin.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from Device.formslist.forms_admin import AdminForm
 from Device.models import UserMst
 import logging
 #------------------------------------------------------------------------------------------------#
@@ -8,4 +9,107 @@ import logging
 # 引数：リクエスト　ユーザー種類
 # 戻り値：なし
 
-# def manage_admin(request, intUsr ):
+def manage_admin(request, intUsr ):
+
+    try:
+            
+            #不正アクセスが起きた場合
+            objuser = UserMst.objects.filter( id = intUsr )          
+            if objuser is None  :
+                
+                # ログイン画面に移行
+                request.session.flush()
+                return redirect( 'login' )
+            
+            blnname         = True
+            blnloginid      = True
+            blnpassword     = True
+            intkind         = True
+            blnerror        = False
+            blnerror_d      = False
+            
+            # 引数で渡すものを指定
+            insuser = UserMst.objects.filter( id = intUsr )   # ユーザー情報
+    
+            # 共通パラメータ定義
+            params = {
+                'User'                      : insuser.first(),     # ユーザー情報
+                'Form'                      : AdminForm(),         # フォーム設定
+                'AccountName'               : blnname,              # アカウント名入力
+                'LoginID'                   : blnloginid,           # ログインID入力
+                'Password'                  : blnpassword,          # パスワード入力
+                'AccountKind'               : intkind,              # アカウント種類
+                'RequiredError'             : blnerror,             # 入力値エラー表示
+                'DuplicateError'            : blnerror_d,           # 重複エラー表示      
+                }
+            
+            # GET時処理
+            if request.method == 'GET':
+    
+                # ホーム画面表示
+                return render( request, 'Manage_Admin.html', params )    
+            
+            # POST時処理
+            if request.method == 'POST':
+    
+                # 登録ボタン押下時
+                if 'btnCreate' in request.POST:
+    
+                    # 未入力がある場合
+                    if ( request.POST['chrLoginID']  == '' or 
+                        request.POST['chrPassWord'] == '' or
+                        request.POST['chrName']     == ''
+                    ):
+                        
+                        blnerror    = True
+    
+                        # パラメータ更新
+                        params['RequiredError'] = blnerror
+    
+                        return render( request, 'Manage_Admin.html', params )    
+    
+                    objuser = None
+                    objuser = UserMst.objects.filter( usrLoginID  = request.POST[ 'chrLoginID' ], 
+                                                    usrPassWord = request.POST[ 'chrPassWord' ], 
+                                                    usrDelete   = False                            
+                                                ).first()
+                    
+                    # 入力に不備がある場合
+                    if objuser is None :   
+                        blnerror_d  = True  
+    
+                        # パラメータ更新
+                        params['DuplicateError'] = blnerror_d
+    
+                        return render( request, 'Manage_Admin.html', params )
+                    # 入力に不備がない場合
+                    else :
+                        
+                        # ホーム画面に移行
+                        strurl = reverse( 'home_admin', kwargs = { 'intUsr' : intUsr } )
+                        return redirect( strurl )
+                    
+                # 戻るボタン押下時
+                if 'btnBack' in request.POST:
+    
+                    # ホーム画面に移行
+                    strurl = reverse( 'home_admin', kwargs = { 'intUsr'  : intUsr } )
+                    return redirect( strurl )
+                
+                # ログアウトボタン押下時
+                if 'btnLogout' in request.POST:
+    
+                    # ログイン画面に移行
+                    strurl = reverse( 'login' )
+                    return redirect( strurl )
+                
+            
+                
+    except:
+        # トレース設定
+        import traceback
+
+        # ログ出力
+        logger = logging.getLogger(__name__)
+        logger.error( request )
+        logger.error( traceback.format_exc() )
